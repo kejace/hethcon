ddaf96e1a800bf686890c529f3f31f791f4f65e9
Martin Allen <blinky3713@gmail.com>
2017-12-30 11:28:50 -0800

    instances for SqlType

--- src/Main.hs
+++ src/Main.hs
@@ -4,21 +4,46 @@
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE DeriveGeneric #-}
 {-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE TypeOperators #-}
+{-# OPTIONS_GHC -fno-warn-orphans #-}
 
 module Main where
 
+import Data.Maybe
+import Database.Selda
+import Database.Selda.Backend
+import GHC.TypeLits
 import Network.Ethereum.Web3
+import Network.Ethereum.Web3.Encoding
 import Network.Ethereum.Web3.TH
+import Network.Ethereum.Web3.Address (toText, fromText)
 
 [abiFrom|data/ERC20.json|]
 
 {-
--- generated event types and instances
+-- generated transfer event type
 
-data Approval
-  = Approval {-# NOUNPACK #-} !Address {-# NOUNPACK #-} !Address {-# NOUNPACK #-} !Integer
-  deriving (Show, Eq, Ord, GHC.Generics.Generic)
-instance Generic Approval
+{ "anonymous": false,
+  "inputs": [
+    {
+      "indexed": true,
+      "name": "_from",
+      "type": "address"
+    },
+    {
+      "indexed": true,
+      "name": "_to",
+      "type": "address"
+    },
+    {
+      "indexed": false,
+      "name": "_value",
+      "type": "uint256"
+    }
+  ],
+  "name": "Transfer",
+  "type": "event"
+}
 
 data Transfer
    = Transfer {-# NOUNPACK #-} !Address {-# NOUNPACK #-} !Address {-# NOUNPACK #-} !Integer
@@ -27,6 +52,28 @@ data Transfer
 
 -}
 
+-- these instances would be defined in a central library
+instance SqlType Address where
+  mkLit = LCustom . LText . toText
+  sqlType _ = TText
+  fromSql (SqlString x) = fromRight . fromText $ x
+  fromSql v          = error $ "fromSql: address column with non-address value: " ++ show v
+  defaultValue = error "No default value for Address type"
+
+instance KnownNat n => SqlType (UIntN n) where
+  mkLit = LCustom . LText . toData
+  sqlType _ = TText
+  fromSql (SqlString x) = fromJust . fromData $ x
+  fromSql v          = error $ "fromSql: int column with non-int value: " ++ show v
+  defaultValue = error "No default value for UIntN type"
+
+-- transfers table definition, possible to generate automatically
+transfers :: Table (Address :*: Address :*: UIntN 256)
+transfers = table "transfer" $ required "_to" :*: required "_from" :*: required "_value"
+
 main :: IO ()
 main = do
   putStrLn "hello world"
+
+fromRight :: Either a b -> b
+fromRight (Right b) = b
