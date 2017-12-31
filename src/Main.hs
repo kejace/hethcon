10410a766e32e6d770a5d672ba2157e3059c1cd8
Martin Allen <blinky3713@gmail.com>
2017-12-31 14:30:58 -0800

    clean up

--- src/Main.hs
+++ src/Main.hs
@@ -6,12 +6,10 @@ module Main where
 
 import qualified Contracts.ERC20           as ERC20
 import           Control.Concurrent        (ThreadId, threadDelay)
--- import           Control.Concurrent.Chan
 import           Database.Selda
 import qualified Database.Selda.Generic    as SG
 import           Database.Selda.PostgreSQL
 import           Network.Ethereum.Web3
--- import           System.IO.Unsafe          (unsafePerformIO)
 
 
 
@@ -22,22 +20,11 @@ import           Orphans                   ()
 transfers :: SG.GenTable ERC20.Transfer
 transfers = SG.genTable "transfer" []
 
---transfersChan :: Chan ERC20.Transfer
---transfersChan = unsafePerformIO newChan
---{-# NOINLINE transfersChan #-}
-
 eventLoop :: PGConnectInfo -> Address -> Web3 HttpProvider ThreadId
 eventLoop conn addr = event addr $ \t@ERC20.Transfer{} -> do
   liftIO . print $ "Got transfer : " ++ show t
   _ <- liftIO . withPostgreSQL conn $ SG.insertGen_ transfers [t]
   return ContinueEvent
-
---transfersConsumer :: PGConnectInfo -> IO ()
---transfersConsumer conn = do
---  t <- readChan transfersChan
---  _ <- liftIO . withPostgreSQL conn $ SG.insertGen_ transfers [t]
---  threadDelay 1000000
---  transfersConsumer conn
 
 main :: IO ()
 main = do
@@ -47,6 +34,7 @@ main = do
     _ <- runWeb3' $ eventLoop pgConn (erc20Address config)
     loop
   where
+    -- this is dumb, but needed to keep the process alive.
     loop = do
       _ <- threadDelay 1000000000
       loop
